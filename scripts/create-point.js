0f6fe3b64f412e14e4e558be2d92bbe86d8c4e64
Artur <arturcastropolonorte@gmail.com>
2020-06-05 07:40:37 -0300

    create point

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -46,3 +46,39 @@ function getCities(event){
 
 document.querySelector("select[name=uf]")
 .addEventListener("change", getCities);
+
+
+//itens de coleta
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (const item of itemsToCollect){
+  item.addEventListener("click", handleSelectedItem);
+
+}
+
+const collectedItems = document.querySelector("input[name=items]");
+
+let selectedItems = [];
+
+function handleSelectedItem(event){
+  const itemLi = event.target;
+  
+  itemLi.classList.toggle("selected")
+
+  const itemId = itemLi.dataset.id;
+
+  const alreadySelected = selectedItems.findIndex(item => item == itemId);
+
+  if( alreadySelected >=0 ){    
+    const filteredItems = selectedItems.filter(item => item != itemId);
+    
+    selectedItems = filteredItems;
+
+  } else{    
+    selectedItems.push(itemId);
+
+  }
+  console.log(selectedItems)
+    
+  collectedItems.value = selectedItems;
+}
